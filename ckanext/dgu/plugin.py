3584438d376aac9866e3e0273141ba52789ad111
Adrià Mercader <amercadero@gmail.com>
2011-05-05 18:24:41 +0100

    Show Gemini links in two columns in resources table if package does not have resources

--- ckanext/dgu/plugin.py
+++ ckanext/dgu/plugin.py
@@ -140,7 +140,12 @@ class FormApiPlugin(SingletonPlugin):
                       limit(1).first()
                 if doc:
                     data = {'guid': doc.guid}
+                    html_code = html.GEMINI_CODE
+                    if len(c.pkg.resources) == 0:
+                        # If no resources, the table has only two columns
+                        html_code = html_code.replace('<td></td>','')
+
                     stream = stream | Transformer('body//div[@class="resources subsection"]/table')\
-                        .append(HTML(html.GEMINI_CODE % data))
+                        .append(HTML(html_code % data))
 
         return stream
