d888b090eec5b80e1fae6b7691ede162a038bac3
Adrià Mercader <amercadero@gmail.com>
2011-11-14 15:35:39 +0000

    [gemini,api] Change the GEMINI links on the dataset page
    The API now uses the harvest_object_id extra from the dataset to
    link to the source GEMINI document.

--- ckanext/dgu/stream_filters.py
+++ ckanext/dgu/stream_filters.py
@@ -8,14 +8,11 @@ from ckanext.harvest.model import HarvestObject
 import ckanext.dgu.forms.html as html
 
 def harvest_filter(stream, pkg):
-    # We need the guid from the HarvestedObject!
-    doc = Session.query(HarvestObject). \
-          filter(HarvestObject.package_id==pkg.id). \
-          order_by(HarvestObject.metadata_modified_date.desc()). \
-          order_by(HarvestObject.gathered.desc()). \
-          limit(1).first()
-    if doc:
-        data = {'guid': doc.guid}
+
+    harvest_object_id = pkg.extras.get('harvest_object_id')
+    if harvest_object_id:
+
+        data = {'id': harvest_object_id}
         html_code = html.GEMINI_CODE
         if len(pkg.resources) == 0:
             # If no resources, the table has only two columns
@@ -23,5 +20,6 @@ def harvest_filter(stream, pkg):
 
         stream = stream | Transformer('body//div[@class="resources subsection"]/table')\
             .append(HTML(html_code % data))
+
     return stream
 
