845073994b24140c1dca3f1843640ff2abb48fbb
Ian Murray <ian.c.murray@gmail.com>
2011-12-08 17:28:08 +0000

    [package form html] First stab at the html for the package form
     - moving around of existing fields into their correct fieldsets
     - relabelling of various fields
     - ripped out the definition list-related tags, <dl>, <dt> and <dd>.

--- ckanext/dgu/controllers/package_gov3.py
+++ ckanext/dgu/controllers/package_gov3.py
@@ -67,7 +67,7 @@ class PackageGov3Controller(PackageController):
         c.publishers = self.get_publishers()
 
         c.is_sysadmin = Authorizer().is_sysadmin(c.user)
-        c.resource_columns = model.Resource.get_columns()
+        c.resource_columns = ('name', 'url')
 
         ## This is messy as auths take domain object not data_dict
         pkg = context.get('package') or c.pkg
