1674a8d4c90c937b5eee706b0101efc7422025db
Ian Murray <ian.murray@okfn.org>
2012-01-23 19:09:18 +0000

    [#1527] Tidied up the theme section of the package form

--- ckanext/dgu/theme_common/public/scripts/dgu-package-form.js
+++ ckanext/dgu/theme_common/public/scripts/dgu-package-form.js
@@ -14,6 +14,8 @@
     $('.js-tooltip-instruction-needed').attr('title', 'Tooltip text required?');
     $('.js-tooltip-instruction-needed').tooltip({'extraClass':'instruction-needed'});
 
+    $('.instruction-needed').tooltip({'extraClass':'instruction-needed'});
+
     $('.to-be-completed').addClass('js-tooltip-to-be-completed');
     $('.js-tooltip-to-be-completed').tooltip({'extraClass':'to-be-completed'});
 
