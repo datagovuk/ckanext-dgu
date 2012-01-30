5673b1c0385925024745e345d1e183841e12ca5d
Ian Murray <ian.murray@okfn.org>
2012-01-30 16:30:37 +0000

    [#1645] Removed the js and css tooltip highlighting.
    No longer form-specific as applying to dgu theme in general.

--- ckanext/dgu/theme_common/public/scripts/dgu-package-form.js
+++ ckanext/dgu/theme_common/public/scripts/dgu-package-form.js
@@ -9,15 +9,6 @@
       $("#title").focus();
     }
 
-    /* Create javascript tooltips */
-    $('.js-tooltip').tooltip();
-    $('.js-tooltip-instruction-needed').attr('title', 'Tooltip text required?');
-    $('.js-tooltip-instruction-needed').tooltip({'extraClass':'instruction-needed'});
-
-    $('.instruction-needed').tooltip({'extraClass':'instruction-needed'});
-
-    $('.to-be-completed').addClass('js-tooltip-to-be-completed');
-    $('.js-tooltip-to-be-completed').tooltip({'extraClass':'to-be-completed'});
 
     /* Toggling visibility of time-series/data resources */
     var toggled = function() {
