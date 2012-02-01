a036fd74573781283a640cdef1097f5f10de7c73
Ian Murray <ian.murray@okfn.org>
2012-02-01 17:09:34 +0000

    [#1527] Prevent the save button appearing without all required fields having values.
    Note - this is *not* the same as having the save button appear when the form is validated.

--- ckanext/dgu/theme_common/public/scripts/dgu-package-form.js
+++ ckanext/dgu/theme_common/public/scripts/dgu-package-form.js
@@ -65,6 +65,17 @@
         nextTab.removeClass('disabled');
       }
     });
+
+    /* Ensure required fields have values before the save button appears */
+    CKAN.Dgu.bindInputChanges($('.field_req'), function() {
+      var complete = true;
+      $('.field_req').each(function(){
+        var v = $(this).val();
+        complete &= Boolean(v);
+      });
+      if(complete){ $('#save-button').removeAttr('disabled'); }
+      else { $('#save-button').attr('disabled', 'disabled'); }
+    });
   });
 }(jQuery));
 
