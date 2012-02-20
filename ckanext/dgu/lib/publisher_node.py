b80c2bee17c60ef4a716e62a0fb73acabe860b87
Ross Jones <rossdjones@gmail.com>
2012-02-20 11:08:32 +0000

    Fix for 500 on resource page

--- ckanext/dgu/lib/publisher_node.py
+++ ckanext/dgu/lib/publisher_node.py
@@ -1,41 +1,42 @@
 import StringIO
-        
+
 class PublisherNode:
     """
     A tree node for rendering publishers in our hierarchy view
     """
-    
+
     def __init__(self,sl, s):
         self.title = s
         self.slug = sl
-        self.children = []        
-        
+        self.children = []
+
     def indent(self,x, txt):
         return ('   ' * x) + txt
-        
+
     def linkify(self):
+        # Should use
+        # h.url_for(controller='ckanext.dgu.controllers.publisher:PublisherController', action='read', id=self.slug)
         return "<a href='/publisher/%s'>%s</a>" % (self.slug,self.title)
-        
-    def render(self):    
+
+    def render(self):
         """
         Renders the output from this tree as a string
-        """ 
+        """
         output = StringIO.StringIO()
         self.format_output( output=output, lvl=0 )
         contents = output.getvalue()
         output.close()
         return contents
-        
-    def format_output(self, output, lvl=0, ):  
+
+    def format_output(self, output, lvl=0, ):
         """
-        Formats the tree in a format that is useful for rendering to 
+        Formats the tree in a format that is useful for rendering to
         jstree.
-        """              
+        """
         output.write( self.indent(lvl,'<ul>') )
         for nx in self.children:
             output.write( self.indent(lvl, "<li id='node_" + nx.slug + "'>" + nx.linkify())   )
             if len(nx.children) > 0 and nx.slug != self.slug:
-                nx.format_output( output, lvl+1)     
+                nx.format_output( output, lvl+1)
             output.write( self.indent(lvl, "</li>") )
         output.write( self.indent(lvl,'</ul>') )
-                
