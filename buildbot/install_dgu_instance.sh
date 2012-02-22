6b5352b84f852029e0ea3f5e691c7e1fa4edffb6
Ian Murray <ian.murray@okfn.org>
2012-02-22 21:30:15 +0000

    Build commands for running the import scripts.

--- buildbot/install_dgu_instance.sh
+++ buildbot/install_dgu_instance.sh
@@ -51,13 +51,16 @@ install_dependencies () {
 
 run_database_migrations () {
     instance=$1
+    users_file=$2
     sudo -u postgres psql -c "UPDATE package_extra SET key = 'UKLP' WHERE key = 'INSPIRE';" "$instance"
     sudo -u postgres psql -c "UPDATE package_extra_revision SET key = 'UKLP' WHERE key = 'INSPIRE';" "$instance"
     sudo -u postgres psql -c "UPDATE resource SET format = NULL where format = 'Unverified';" "$instance"
     sudo -u postgres psql -c "UPDATE resource_revision SET format = NULL where format = 'Unverified';" "$instance"
 
     pyenv_root="/var/lib/ckan/$instance/pyenv"
-    "$pyenv_root/bin/python" "$pyenv_root/src/ckanext-dgu/ckanext/dgu/bin/import_publishers.py" "/etc/ckan/$instance/$instance.ini" "$pyenv_root/src/ckanext-dgu/buildbot/fixtures/dgupub.csv"
+    "$pyenv_root/bin/python" "$pyenv_root/src/ckanext-dgu/ckanext/dgu/bin/import_publishers.py" "/etc/ckan/$instance/$instance.ini" "$pyenv_root/src/ckanext-dgu/buildbot/etc/dgupub.csv"
+    "$pyenv_root/bin/python" "$pyenv_root/src/ckanext-dgu/ckanext/dgu/bin/publisher_datasets_assoc.py" "/etc/ckan/$instance/$instance.ini" "$pyenv_root/src/ckanext-dgu/buildbot/etc/nodepublishermap.csv" | sudo -u postgres psql "$instance"
+    #"$pyenv_root/bin/python" "$pyenv_root/src/ckanext-dgu/ckanext/dgu/bin/user_import.py" "/etc/ckan/$instance/$instance.ini" "$pyenv_root/src/ckanext-dgu/buildbot/etc/nodepublishermap.csv" "$users_file"
 
 }
 
