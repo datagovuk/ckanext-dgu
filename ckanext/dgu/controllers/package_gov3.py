07e7cbf41fa64533bf3ee3023ab72c5e7d61925e
Ian Murray <ian.murray@okfn.org>
2012-02-23 17:28:27 +0000

    Don't pre-select a publisher by default, and enforce one must be selected

--- ckanext/dgu/controllers/package_gov3.py
+++ ckanext/dgu/controllers/package_gov3.py
@@ -147,7 +147,7 @@ class PackageGov3Controller(PackageController):
             'individual_resources': individual_resource_schema(),
             
             'groups': {
-                'name': [not_empty, unicode]
+                'name': [not_empty, val.group_id_or_name_exists, unicode]
             },
 
             'contact-name': [unicode, drop_if_same_as_publisher, convert_to_extras],
