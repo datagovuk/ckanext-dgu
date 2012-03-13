088fd04517ef5e511618ba848c4cc093510c05cb
Ian Murray <ian.murray@okfn.org>
2012-03-13 11:40:15 +0000

    Removed the unecessary FormApiPlugin

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(
     package_data={'ckan': ['i18n/*/LC_MESSAGES/*.mo']},
     entry_points="""
         [ckan.plugins]
-        dgu_form_api = ckanext.dgu.plugin:FormApiPlugin
         dgu_form = ckanext.dgu.plugin:DguForm
         dgu_auth_api = ckanext.dgu.plugin:AuthApiPlugin
         form_api_tester = ckanext.dgu.testtools.form_api_tester:FormApiTester
