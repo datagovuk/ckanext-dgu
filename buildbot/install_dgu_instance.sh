cfc1e196c232dd31afd703c9660eb5df85b021d0
Ian Murray <ian.murray@okfn.org>
2012-03-21 12:34:19 +0000

    Added missing ckanext-spatial plugins

--- buildbot/install_dgu_instance.sh
+++ buildbot/install_dgu_instance.sh
@@ -92,7 +92,7 @@ configure () {
     ini_file="/etc/ckan/$instance/$instance.ini"
 
     # Configures the ini file settings
-    sudo sed -e "s/ckan.plugins =.*$/ckan.plugins = dgu_publisher_form dgu_publishers dgu_auth_api dgu_form dgu_theme cswserver harvest gemini_harvester gemini_doc_harvester gemini_waf_harvester inspire_api wms_preview spatial_query qa synchronous_search dgu_search dgu_dataset_form/" \
+    sudo sed -e "s/ckan.plugins =.*$/ckan.plugins = dgu_publisher_form dgu_publishers dgu_auth_api dgu_form dgu_theme cswserver harvest gemini_harvester gemini_doc_harvester gemini_waf_harvester inspire_api wms_preview spatial_query qa synchronous_search dgu_search dgu_dataset_form spatial_metadata dataset_extent_map/" \
              -e "s/^ckan.site_title =.*/ckan.site_title = DGU Release Test/" \
              -e "s/^ckan.site_url =.*/ckan.site_url = http:\/\/$domain/" \
              -e "s/^ckan.gravatar_default =.*/ckan.gravatar_default = mm/" \
