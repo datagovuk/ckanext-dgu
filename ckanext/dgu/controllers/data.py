5187ffc4b88779a2b26f89f1ea5782aa90c0ad50
David Read <david.read@okfn.org>
2012-03-21 16:13:49 +0000

    Setup data page with basic content and as the homepage. Moved tag, package search & api routes under /data, but left /dataset/<id> route so that the paths do not change from previous site. Made login/logout functional. Number of search results in search_form is now live (for both /data/search and /data pages).

---
+++ ckanext/dgu/controllers/data.py
@@ -0,0 +1,36 @@
+import ckan.logic
+from ckan.lib.base import request, c, BaseController, model, abort, h, g, render
+#from ckan.controllers.package import PackageController
+
+class DataController(BaseController):
+    def index(self):
+        from ckan.lib.search import SearchError
+        try:
+            # package search
+            context = {'model': model, 'session': model.Session,
+                       'user': c.user or c.author}
+            data_dict = {
+                'q':'*:*',
+                'facet.field':g.facets,
+                'rows':0,
+                'start':0,
+            }
+            query = ckan.logic.get_action('package_search')(context,data_dict)
+            c.package_count = query['count']
+            c.facets = query['facets']
+
+            # group search
+            data_dict = {'order_by': 'packages', 'all_fields': 1}
+            c.groups = ckan.logic.get_action('group_list')(context, data_dict)
+        except SearchError, se:
+            c.package_count = 0
+            c.groups = []
+
+        c.recently_changed_packages_activity_stream = \
+            ckan.logic.action.get.recently_changed_packages_activity_list_html(
+                    context, {})
+
+        return render('data/index.html')
+
+    def api(self):
+        return render('data/api.html')
