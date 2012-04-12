c899d51de3eb73190d8e2cd935b314372254a92b
Tom Rees <zephod@gmail.com>
2012-04-12 20:04:05 +0100

    Basket model/view established.

--- ckanext/dgu/theme/public/scripts/dgu-basket.js
+++ ckanext/dgu/theme/public/scripts/dgu-basket.js
@@ -2,31 +2,66 @@ $(function() {
 
   var basketUi = $('#shopping-basket');
   var basketSubmit = $('#shopping-basket-submit');
+  var basket = [];
+
+  var renderBasket = function() {
+    basketUi.empty();
+    $.each(basket, function(i,item) {
+      var li = $('<li/>').html(item.title).attr('id',item.id);
+      li.appendTo(basketUi);
+    });
+    if (basket.length==0) {
+      var li = $('<li/>').html("No items").addClass('empty');
+      li.appendTo(basketUi);
+    }
+  };
+
+  var addToBasket = function(id, title) {
+    basket.push({
+      id: id,
+      title: title
+    });
+  };
+  var removeFromBasket = function(id) {
+    var index = -1;
+    $.each(basket, function(i,item) {
+      if (item.id == id) {
+        index = i;
+      }
+    });
+    if (index==-1) throw "Item not found.";
+    basket.splice(index,1);
+  };
 
   var previewAdd = function(e) {
-    e.preventDefault();
     var parent = $(e.target).parents('.dataset');
     var packageId = parent.find('.js-data-id').html();
     var packageTitle = parent.find('.js-data-title').html();
-    var li = $('<li/>').addClass('entry').html(packageTitle).attr('id',packageId);
-    // Add an entry to the shopping basket
-    if (!basketUi.find('#'+packageId).length) {
-      li.appendTo(basketUi);
-    }
-    basketUi.find('.empty').hide();
+    addToBasket(packageId,packageTitle);
+    // Update my UI
+    parent.find('.preview-add').hide();
+    parent.find('.preview-remove').show();
+    renderBasket();
+  };
+
+  var previewRemove = function(e) {
+    var parent = $(e.target).parents('.dataset');
+    var packageId = parent.find('.js-data-id').html();
+    removeFromBasket(packageId);
+    // Update my UI
+    parent.find('.preview-add').show();
+    parent.find('.preview-remove').hide();
+    renderBasket();
   };
 
   var basketSubmit = function(e) {
-    e.preventDefault();
-    var ids = [];
-    $.each(basketUi.find('.entry'), function(i, li) {
-      ids.push( li.attr('id') );
-    });
-    console.log(ids);
+    console.log(basket);
   };
 
-  $('.preview-add').bind('click',previewAdd);
+  $('.preview-add button').bind('click',previewAdd);
+  $('.preview-remove button').bind('click',previewRemove);
   basketSubmit.bind('click',basketSubmit);
+  renderBasket();
 });
 
 
