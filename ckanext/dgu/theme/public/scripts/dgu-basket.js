1320926272e871ca631536da12e5b9c6b591d3e2
Tom Rees <zephod@gmail.com>
2012-04-13 15:24:04 +0100

    Further styling of the shopping basket.

--- ckanext/dgu/theme/public/scripts/dgu-basket.js
+++ ckanext/dgu/theme/public/scripts/dgu-basket.js
@@ -1,5 +1,6 @@
 $(function() {
 
+  var basketUiContainer = $('#shopping-basket-container');
   var basketUi = $('#shopping-basket');
   var basketSubmitButton = $('#shopping-basket-submit');
   var basket = [];
@@ -8,11 +9,15 @@ $(function() {
     basketUi.empty();
     $.each(basket, function(i,item) {
       var li = $('<li/>').html(item.title).attr('id',item.id);
+      var xButton = $('<button/>').addClass('btn').addClass('btn-small').addClass('x-button').html('x');
+      xButton.prependTo(li);
       li.appendTo(basketUi);
     });
-    if (basket.length==0) {
-      var li = $('<li/>').html("No items").addClass('empty');
-      li.appendTo(basketUi);
+    if (basket.length==0 && basketUiContainer.is(':visible')) {
+      basketUiContainer.hide('slow')
+    }
+    if (basket.length>0 && !basketUiContainer.is(':visible')) {
+      basketUiContainer.show('slow')
     }
   };
 
@@ -34,12 +39,28 @@ $(function() {
     basket.splice(index,1);
   };
 
-  var previewAdd = function(e) {
+  var clickAdd = function(e) {
     var parent = $(e.target).parents('.dataset');
     var packageId = parent.find('.js-data-id').html();
     var packageTitle = parent.find('.js-data-title').html();
     var packageQuerystring = parent.find('.js-data-querystring').html();
     addToBasket(packageId,packageTitle,packageQuerystring);
+    // Trial animation
+    /*
+    var fromImg = parent.find('.preview-add img');
+    var from = fromImg.offset();
+    var to = basketUi.offset();
+    to.left += (basketUi.width() / 2)-20;
+    to.top += (basketUi.height() / 2)-20;
+    to.opacity = 0.75;
+    var img = $('<img/>').attr('src','/images/compass.png').css({ 'position':'absolute' }).css(from);
+    img.appendTo($('body'));
+    img.animate(to, 800, "easeOutCubic", function() {
+      img.animate({'opacity':0}, 200, "linear", function() {
+        img.remove();
+      });
+    });
+    */
     // Update my UI
     parent.find('.preview-add').hide();
     parent.find('.preview-remove').show();
@@ -55,7 +76,7 @@ $(function() {
     renderBasket();
   };
 
-  var previewRemove = function(e) {
+  var clickRemove = function(e) {
     var parent = $(e.target).parents('.dataset');
     var packageId = parent.find('.js-data-id').html();
     removeFromBasket(packageId);
@@ -64,8 +85,21 @@ $(function() {
     parent.find('.preview-remove').hide();
     renderBasket();
   };
+  
+  var clickX = function(e) {
+    var id = $(e.target).parents('li').attr('id');
+    // Find the button within the page
+    var mainButton = $('.js-dataset-'+id+' .preview-remove button');
+    if (mainButton.length) {
+      mainButton.click();
+    }
+    else {
+      removeFromBasket(id);
+      renderBasket();
+    }
+  };
 
-  var basketSubmit = function(e) {
+  var clickSubmit = function(e) {
     e.preventDefault();
     var href = $(e.target).attr('href');
     if (basket.length) {
@@ -76,10 +110,11 @@ $(function() {
     }
   };
 
-  $('.preview-add button').bind('click',previewAdd);
-  $('.preview-remove button').bind('click',previewRemove);
-  basketSubmitButton.bind('click',basketSubmit);
+  $('.preview-add button').bind('click',clickAdd);
+  $('.preview-remove button').bind('click',clickRemove);
+  basketSubmitButton.bind('click',clickSubmit);
   renderBasket();
+  $('#shopping-basket .x-button').live('click', clickX);
 });
 
 
