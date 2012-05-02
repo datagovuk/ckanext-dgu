9d786812c643f9c3bbd1d43771e7c7f946b0b4d4
David Read <david.read@hackneyworkshop.com>
2012-05-02 19:10:12 +0100

    Fix dgu_auth_tkt startup

--- ckanext/dgu/authentication/auth_tkt.py
+++ ckanext/dgu/authentication/auth_tkt.py
@@ -7,7 +7,7 @@ DGU modifications:
 import sys
 import logging
 
-from repoze.who.plugins.auth_tkt import AuthTktCookiePlugin
+from repoze.who.plugins.auth_tkt import AuthTktCookiePlugin, _bool
 
 log = logging.getLogger(__name__)
 
@@ -22,3 +22,38 @@ class DGUAuthTktCookiePlugin(AuthTktCookiePlugin):
         frame = sys._getframe(n)
         c = frame.f_code
         return c.co_filename, c.co_name
+
+def make_plugin(secret=None,
+                secretfile=None,
+                cookie_name='auth_tkt',
+                secure=False,
+                include_ip=False,
+                timeout=None,
+                reissue_time=None,
+                userid_checker=None,
+               ):
+    from repoze.who.utils import resolveDotted
+    if (secret is None and secretfile is None):
+        raise ValueError("One of 'secret' or 'secretfile' must not be None.")
+    if (secret is not None and secretfile is not None):
+        raise ValueError("Specify only one of 'secret' or 'secretfile'.")
+    if secretfile:
+        secretfile = os.path.abspath(os.path.expanduser(secretfile))
+        if not os.path.exists(secretfile):
+            raise ValueError("No such 'secretfile': %s" % secretfile)
+        secret = open(secretfile).read().strip()
+    if timeout:
+        timeout = int(timeout)
+    if reissue_time:
+        reissue_time = int(reissue_time)
+    if userid_checker is not None:
+        userid_checker = resolveDotted(userid_checker)
+    plugin = DGUAuthTktCookiePlugin(secret,
+                                 cookie_name,
+                                 _bool(secure),
+                                 _bool(include_ip),
+                                 timeout,
+                                 reissue_time,
+                                 userid_checker,
+                                 )
+    return plugin
