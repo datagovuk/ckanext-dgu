38a213086a2fd444b5e1377493d42a5a2575d015
David Read <david.read@hackneyworkshop.com>
2012-05-17 11:01:17 +0100

    Moved auth.py to authorize.py to differentiate it from authenticate. Removed publisher sync code.

--- ckanext/dgu/plugin.py
+++ ckanext/dgu/plugin.py
@@ -14,7 +14,7 @@ from ckan.plugins import IAuthFunctions
 from ckan.plugins import IPackageController
 from ckan.plugins import ISession
 from ckanext.dgu.authentication.drupal_auth import DrupalAuthMiddleware
-from ckanext.dgu.auth import dgu_group_update, dgu_group_create, \
+from ckanext.dgu.authorize import dgu_group_update, dgu_group_create, \
                              dgu_package_update, dgu_extra_fields_editable, \
                              dgu_dataset_delete
 from ckan.lib.helpers import url_for
