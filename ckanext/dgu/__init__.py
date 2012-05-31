257efd066dfbcfb06539e2b8add5190aea8a10f4
Tom Rees <zephod@gmail.com>
2012-05-31 18:44:14 +0100

    Restored monkey-patch to somewhere a little safer.

--- ckanext/dgu/__init__.py
+++ ckanext/dgu/__init__.py
@@ -8,22 +8,3 @@ except ImportError:
 
 __version__ = '0.4'
 
-# == Monkey Patching ==
-def dgu_linked_user(user):  # Overwrite h.linked_user
-    from ckan import model
-    if user in [model.PSEUDO_USER__LOGGED_IN, model.PSEUDO_USER__VISITOR]:
-        return user
-    if not isinstance(user, model.User):
-        user_name = unicode(user)
-        user = model.User.get(user_name)
-        if not user:
-            return '(no id)'
-    if user:
-        _name = user.id
-        _icon = gravatar(None, 20)
-        return _icon + link_to(_name,
-                       url_for(controller='user', action='read', id=_name))
-#Disabled for now as it borks install
-#from ckan.lib.base import h
-#h.linked_user = dgu_linked_user
-
