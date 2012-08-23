53077b313e59e2b95fc4ea951542a958bd8ecb97
Tom Rees <zephod@gmail.com>
2012-08-23 18:47:25 +0100

    Switch to scrollbars

--- ckanext/dgu/theme/public/scripts/dgu-publisher.js
+++ ckanext/dgu/theme/public/scripts/dgu-publisher.js
@@ -6,30 +6,8 @@ $(function() {
   jstree.bind('loaded.jstree', function() { 
     var li = $('.jstree li');
     var active = $( $('.jstree li strong').parents('li')[0] );
-    var activeIndex = li.index(active);
-
-    if (li.length>15) {
-      var treeWrapper = $('#publisher-tree-wrapper');
-      var treeButton = $('#publisher-tree-expand');
-      var hidden=false;
-      $.each(li, function(i, element) {
-        if (i>activeIndex+2) {
-          $(element).hide();
-          hidden=true;
-        }
-      });
-
-      if (hidden) {
-        treeButton.show();
-        treeButton.click(function() {
-          treeButton.hide();
-          $.each(li, function(i,element) { $(element).show(); });
-        });
-      }
-    }
-  });
-  jstree.bind('open_node.jstree', function(e) {
-    $('.jstree-open').show();
+    var offset = Math.max(0, active.offset().top - jstree.offset().top - 20);
+    jstree.scrollTop(offset);
   });
 
   jstree.jstree(
