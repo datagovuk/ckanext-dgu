a2d2967352230d65880c9233028f75afd3beeb7d
Tom Rees <zephod@gmail.com>
2012-08-23 18:49:02 +0100

    Alter height metrics

--- ckanext/dgu/theme/public/scripts/dgu-publisher.js
+++ ckanext/dgu/theme/public/scripts/dgu-publisher.js
@@ -6,7 +6,7 @@ $(function() {
   jstree.bind('loaded.jstree', function() { 
     var li = $('.jstree li');
     var active = $( $('.jstree li strong').parents('li')[0] );
-    var offset = Math.max(0, active.offset().top - jstree.offset().top - 20);
+    var offset = Math.max(0, active.offset().top - jstree.offset().top - 80);
     jstree.scrollTop(offset);
   });
 
