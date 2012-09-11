5caa2d2b1b15eed0cf2f9ed8817f67085ea2d880
David Read <david.read@hackneyworkshop.com>
2012-09-11 16:21:47 +0100

    When not authorized, send to a page offering to login in Drupal, rather than the CKAN login form.

---
+++ ckanext/dgu/authentication/drupal_repoze_plugin.py
@@ -0,0 +1,19 @@
+from zope.interface import implements
+from webob import Request, Response
+
+from repoze.who.interfaces import IChallenger
+
+from ckanext.dgu.plugins_toolkit import render_snippet
+
+class DrupalLoginPlugin(object):
+
+    implements(IChallenger)
+
+    # Catches 401 redirects and sends user to Drupal log-in form 
+    def challenge(self, environ, status, app_headers, forget_headers):
+        # redirect to login_form
+        res = Response()
+        res.status = 401
+        res.unicode_body = render_snippet('not_authorized.html')
+        #res.location = '/data/not_authorized' #self.login_form_url+"?%s=%s" %(self.came_from_field, request.url)
+        return res
