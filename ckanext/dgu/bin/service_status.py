3cb18034dcae3909af3e451578117b5fbf1b1ebd
David Read <david.read@hackneyworkshop.com>
2012-09-17 16:55:54 +0100

    Added service status script.

---
+++ ckanext/dgu/bin/service_status.py
@@ -0,0 +1,104 @@
+# Reports on a DGU server\'s status
+
+import subprocess
+import os
+import ConfigParser
+import re
+
+def check_archiver():
+    print '\n** Archiver **'
+    check_supervisord('dgu')
+
+def check_supervisord(config):
+    check_process('/usr/bin/supervisord')
+    celery_config = ConfigParser.ConfigParser()
+    if config == 'dgu':
+      config_file = '/etc/supervisor/conf.d/celery-supervisor-dgu.conf'
+      config_section = 'program:celery-dgu'
+    else:
+      assert 0, 'Unknown config: %s' % config
+    celery_config.read(config_file)
+    celery_command = celery_config.get(config_section, 'command')
+    print 'Celery command: %s' % celery_command
+    celery_logfile_name = celery_config.get(config_section, 'stderr_logfile')
+    print 'Celery logfile: %s' % celery_logfile_name
+    logfile_tail(celery_logfile_name)
+
+    config_filepath = extract_ckan_config_from_commandline(celery_command)
+    ckan_log_filepath = extract_ckan_log_filepath(config_filepath)
+    logfile_tail(ckan_log_filepath, 'archiver')
+
+def extract_ckan_log_filepath(config_filepath):
+    ckan_config = ConfigParser.ConfigParser()
+    ckan_config.read(config_filepath)
+    args_line = ckan_config.get('handler_file', 'args')
+    match = re.match('\(\"([^"]*)\",.*\)', args_line)
+    if match:
+        log_filepath = match.groups()[0]
+        print 'CKAN logfile: %s' % log_filepath
+        return log_filepath
+    else:
+        print 'Could not extract log filename from: %s' % args_line
+
+def extract_ckan_config_from_commandline(commandline):
+    match = re.search('--config=([^\s]+)', commandline)
+    if match:
+        config_filepath = match.groups()[0]
+        print 'CKAN config: %s' % config_filepath
+        return config_filepath
+    print 'ERROR: Could not match a config file in the commandline: %s' % commandline
+
+def logfile_tail(log_filepath, grep=None):
+    max_lines = 10
+    if not os.path.exists(log_filepath):
+        print 'ERROR Log file doesn\'t exist: %s' % log_filepath
+        return
+    if not grep:
+        p2 = subprocess.Popen(['tail', '-n', str(max_lines), log_filepath], stdout=subprocess.PIPE)
+    else:
+        p1 = subprocess.Popen(['grep', grep, log_filepath], stdout=subprocess.PIPE)
+        p2 = subprocess.Popen(['tail', '-n', str(max_lines)], stdin=p1.stdout, stdout=subprocess.PIPE)
+        p1.stdout.close()
+    lines = p2.communicate()[0].split('\n')
+    for line in lines:
+        print '> ' + line
+
+    
+
+def tail(f, window=20):
+    BUFSIZ = 1024
+    f.seek(0, 2)
+    bytes = f.tell()
+    size = window
+    block = -1
+    data = []
+    while size > 0 and bytes > 0:
+        if (bytes - BUFSIZ > 0):
+            # Seek back one whole BUFSIZ
+            f.seek(block*BUFSIZ, 2)
+            # read BUFFER
+            data.append(f.read(BUFSIZ))
+        else:
+            # file too small, start from begining
+            f.seek(0,0)
+            # only read what was not read
+            data.append(f.read(bytes))
+        linesFound = data[-1].count('\n')
+        size -= linesFound
+        bytes -= BUFSIZ
+        block -= 1
+    return ''.join(data).splitlines()[-window:]
+
+processes = None
+def check_process(process_text):
+    global processes
+    if not processes:
+        processes = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE).communicate()[0]
+    for process in processes.split('\n'):
+        if process_text in process:
+            print 'Process %s is running: %s' % (process_text, process)
+            return
+    print 'ERROR: Process %s is not running' % process_text
+
+if __name__ == '__main__':
+    check_archiver()
