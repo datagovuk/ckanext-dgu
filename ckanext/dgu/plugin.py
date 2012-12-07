17767dc1652f84e8aee253dc103dd608a8ea3968
David Read <david.read@hackneyworkshop.com>
2012-12-07 18:17:38 +0000

    Spatial search fix - e.g. /feeds/group/scottish-government-spatial-data-infrastructure.atom

--- ckanext/dgu/plugin.py
+++ ckanext/dgu/plugin.py
@@ -309,7 +309,7 @@ class SearchPlugin(SingletonPlugin):
         #    ckanext/dgu/theme/templates/package/search.html
         order_by = search_params.get('sort')
         bbox = search_params.get('extras', {}).get('ext_bbox')
-        search_params_apart_from_bbox = search_params['q'] + search_params['fq']
+        search_params_apart_from_bbox = search_params.get('q', '') + search_params.get('fq', '')
         sort_by_location_enabled = bool(bbox and not search_params_apart_from_bbox)
 
         if order_by in (None, 'spatial desc') and sort_by_location_enabled:
