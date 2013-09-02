170b10a57673353c6e75c6747b8ef8034269531b
David Read <david.read@hackneyworkshop.com>
2013-09-02 09:59:05 +0100

    [642] Fix for display of dataset/new.

--- ckanext/dgu/lib/helpers.py
+++ ckanext/dgu/lib/helpers.py
@@ -1477,6 +1477,9 @@ def unpublished_release_date(package):
 
 
 def is_unpublished_item(package):
+    if not package:
+        # e.g. when displaying package/new form
+        return False
     from paste.deploy.converters import asbool
     return asbool(get_from_flat_dict(package['extras'], 'unpublished'))
 
