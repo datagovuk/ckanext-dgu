6a005a75a7aef8d75d1b9b12036d2340ebeb41b8
David Read <david.read@hackneyworkshop.com>
2013-10-15 16:20:06 +0000

    Update the SOLR escape characters for SOLR 4. It was failing when you search for ":/".

--- ckanext/dgu/lib/search.py
+++ ckanext/dgu/lib/search.py
@@ -1,11 +1,11 @@
 import re
 
-# Escape Lucene special characters: + - ! ( ) { } [ ] ^ " ~ * ? : \
+# Escape Lucene special characters: + - ! ( ) { } [ ] ^ " ~ * ? : \ /
 # There are also operators && and ||, but we're just going to escape
 # the individual ampersand and pipe chars.
 # Also, we're not going to escape backslashes!
-# http://lucene.apache.org/java/2_9_1/queryparsersyntax.html#Escaping+Special+Characters
-ESCAPE_CHARS_RE = re.compile(r'(?<!\\)(?P<char>[&|+\-!(){}[\]^"~*?:])')
+# http://lucene.apache.org/core/4_0_0/queryparser/org/apache/lucene/queryparser/classic/package-summary.html#Escaping%20Special%20Characters
+ESCAPE_CHARS_RE = re.compile(r'(?<!\\)(?P<char>[&|+\-!(){}[\]^"~*?:/])')
 
 def solr_escape(value):
     r"""Escape un-escaped special characters and return escaped value.
