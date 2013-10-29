f647e25b48e566fae918d3b16680450b7bc1f1bb
Tom Rees <zephod@gmail.com>
2013-10-29 23:10:16 +0000

    [903] Publishers index page. IE7 works.

--- ckanext/dgu/theme/src/scripts/dgu-publisher-index.js
+++ ckanext/dgu/theme/src/scripts/dgu-publisher-index.js
@@ -1,4 +1,97 @@
 $(function() {
-  console.log('ready to run publisher search');
-});
+  function buildIndex(parentDiv) {
+    var out = []
+    parentDiv.find('> .publisher').each(function(i,row) {
+      row = $(row);
+      out.push({
+        div        : row,
+        link       : row.find('> a:first'),
+        rawtext    : row.find('> a:first').text(),
+        searchtext : row.find('> a:first').text().toLowerCase(),
+        children   : buildIndex(row),
+      });
+    });
+    return out;
+  }
 
+  var container = $('.publisher-hierarchy');
+  var searchBox = $('input#js-search');
+  var resultCountBox = $('.result-count');
+  var resultCountFooterBox = $('.result-count-footer');
+  var index = buildIndex(container);
+  assert (container.length);
+  assert (searchBox.length);
+
+  /* This is effectively a global variable. I am not proud */
+  /* TODO replace with multiple returns from the recursive function 
+   * AS LONG AS it doesn't damage performance.  */
+  var hacky_count = -1;
+  /* Recursive. Runs through a row of the index */
+  function updateSearch(searchString) {
+    searchString = searchString.toLowerCase();
+    if (searchString.length==0 || searchString=='search for publishers...') {
+      var p = $('.publisher');
+      hacky_count = p.length;
+      p.removeClass('match');
+      p.removeClass('childMatch');
+      container.addClass('empty-search');
+      resultCountBox.text(hacky_count);
+      resultCountFooterBox.text('Publishers');
+      return;
+    }
+    container.removeClass('empty-search');
+    hacky_count = 0;
+    for (var i=0;i<index.length;i++) {
+      updateSearch_recur(searchString,index[i]);
+    }
+    resultCountBox.text(hacky_count);
+    resultCountFooterBox.text('Results');
+  }
+  function updateSearch_recur(searchString,entry) {
+    var childMatch = false;
+    for (var i=0;i<entry['children'].length;i++) {
+      childMatch |= updateSearch_recur(searchString,entry['children'][i]);
+    }
+    var offset = entry['searchtext'].indexOf(searchString);
+    if (offset != -1) {
+      hacky_count += 1;
+      /* light them up */
+      var a = entry['rawtext'].substring(0,offset);
+      var b = entry['rawtext'].substring(offset,offset+searchString.length);
+      var c = entry['rawtext'].substring(offset+searchString.length);
+      entry['div'].addClass('match');
+      entry['link'].html(a+'<span class="highlight">'+b+'</span>'+c);
+      return true;
+    }
+    /* no match */
+    entry['div'].removeClass('match');
+    entry['link'].html(entry['rawtext']);
+    if (childMatch) {
+      entry['div'].addClass('childmatch');
+    }
+    else {
+      entry['div'].removeClass('childmatch');
+    }
+    return childMatch;
+  }
+
+
+  /* Bind to search box, call updateSearch when stuff gets typed... */
+  var cacheVal = '';
+  function onChange(e) {
+    var val = searchBox.val();
+    if (val==cacheVal) { return; }
+    cacheVal = val;
+    searchBox.addClass('loading');
+    window.clearTimeout(window.dgu_pub_timeout);
+    window.dgu_pub_timeout = window.setTimeout(function() {
+      searchBox.removeClass('loading');
+      updateSearch(val);
+    }, 200);
+  }
+  searchBox.on('keydown',onChange);
+  searchBox.on('keyup',onChange);
+  searchBox.on('keypress',onChange);
+  searchBox.change(onChange);
+  updateSearch(searchBox.val());
+});
