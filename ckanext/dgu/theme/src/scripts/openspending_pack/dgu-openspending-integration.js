b4de549f3ca90657369bfce7fc447bdf71ae6ed5
Ross Jones <ross@servercode.co.uk>
2013-11-07 13:51:03 +0000

    Initialise openspending pagination on sidebar init

--- ckanext/dgu/theme/src/scripts/openspending_pack/dgu-openspending-integration.js
+++ ckanext/dgu/theme/src/scripts/openspending_pack/dgu-openspending-integration.js
@@ -24,7 +24,7 @@ $(function() {
           moreButton.insertAfter(table);
           moreButton.on('click',function(e) {
               e.preventDefault();
-              $.each(table.find('tr'), function(i,row) { 
+              $.each(table.find('tr'), function(i,row) {
                   $(row).show('slow');
               });
               table.removeClass('trimmed');
@@ -32,11 +32,19 @@ $(function() {
               return false;
           });
       }
+
       function onRedrawSidebar(e) {
           $.each(sidebar.find('table.facets'), function(i,table) {
               var length = $(table).find('tr').length;
               if (length>=8) { limitTableLength($(table)); }
           });
       }
+
+      function initPagination(e){
+        $('.pagination ul').addClass('pagination');
+        $('.pagination').parent().addClass('dgu-pagination');
+      }
+
       sidebar.on('faceter:init faceter:addFilter faceter:removeFilter', onRedrawSidebar);
+      sidebar.on('faceter:init', initPagination);
 });
