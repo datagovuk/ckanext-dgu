dec8a3da58d38d2dbc233e4f8a48af91f6d590f4
David Read <david.read@hackneyworkshop.com>
2013-12-18 17:57:32 +0000

    [1245] ONS loader - log exceptions rather than quitting without trace. Try to add a user agent to ckanclient requests (onyl works if you have the latest ckanclient commit).

--- ckanext/dgu/ons/command.py
+++ ckanext/dgu/ons/command.py
@@ -1,9 +1,10 @@
 import datetime
 
-from ckanclient import CkanClient
 from ckanext.importlib.api_command import ApiCommand
 
 class OnsLoaderCmd(ApiCommand):
+    user_agent = 'ONS Loader'
+
     def add_options(self):
         ApiCommand.add_options(self)
         self.parser.add_option("-d", "--days",
@@ -48,45 +49,51 @@ class OnsLoaderCmd(ApiCommand):
 
         ApiCommand.command(self)
 
-        if self.options.days:
-            self.options.days = int(self.options.days)
-        if self.options.start_date:
-            self.options.start_date = self.parse_date(self.options.start_date)
-        if self.options.end_date:
-            self.options.end_date = self.parse_date(self.options.end_date)
-        if self.options.month:
-            self.options.month = self.parse_month(self.options.month)
-        if self.options.months_since:
-            self.options.months_since = self.parse_month(self.options.months_since)
-        if not self.options.ons_cache_dir:
-            self.options.ons_cache_dir = ONS_DEFAULT_CACHE_PATH
+        try:        
+            if self.options.days:
+                self.options.days = int(self.options.days)
+            if self.options.start_date:
+                self.options.start_date = self.parse_date(self.options.start_date)
+            if self.options.end_date:
+                self.options.end_date = self.parse_date(self.options.end_date)
+            if self.options.month:
+                self.options.month = self.parse_month(self.options.month)
+            if self.options.months_since:
+                self.options.months_since = self.parse_month(self.options.months_since)
+            if not self.options.ons_cache_dir:
+                self.options.ons_cache_dir = ONS_DEFAULT_CACHE_PATH
 
-        if self.options.days or \
-               self.options.start_date or \
-               self.options.end_date:
-            data_filepaths = OnsData.download_flexible(
-                days=self.options.days,
-                start_date=self.options.start_date,
-                end_date=self.options.end_date,
-                ons_cache_dir=self.options.ons_cache_dir)
+            if self.options.days or \
+                self.options.start_date or \
+                self.options.end_date:
+                data_filepaths = OnsData.download_flexible(
+                    days=self.options.days,
+                    start_date=self.options.start_date,
+                    end_date=self.options.end_date,
+                    ons_cache_dir=self.options.ons_cache_dir)
 
-        elif self.options.month:
-            data_filepaths = OnsData.download_month(year=self.options.month.year,
-                                                    month=self.options.month.month)
-        elif self.options.months_since:
-            data_filepaths = OnsData.download_months_since(
-                year=self.options.months_since.year,
-                month=self.options.months_since.month,
-                force_download=self.options.force_download)
-        elif self.options.all_time:
-            data_filepaths = OnsData.download_all(force_download=self.options.force_download)
-        else:
-            self.parser.error('Please specify a time period')
+            elif self.options.month:
+                data_filepaths = OnsData.download_month(year=self.options.month.year,
+                                                        month=self.options.month.month)
+            elif self.options.months_since:
+                data_filepaths = OnsData.download_months_since(
+                    year=self.options.months_since.year,
+                    month=self.options.months_since.month,
+                    force_download=self.options.force_download)
+            elif self.options.all_time:
+                data_filepaths = OnsData.download_all(force_download=self.options.force_download)
+            else:
+                self.parser.error('Please specify a time period')
 
-        importer = OnsImporter(filepaths=data_filepaths, ckanclient=self.client)
-        loader = OnsLoader(self.client)
+            importer = OnsImporter(filepaths=data_filepaths, ckanclient=self.client)
+            loader = OnsLoader(self.client)
 
-        loader.load_packages(importer.pkg_dict())
+            loader.load_packages(importer.pkg_dict())
+        except:
+            # Any problem, make sure it gets logged
+            import logging
+            logging.getLogger(__name__).exception('ONS Loader exception')
+            raise
 
 def load():
     OnsLoaderCmd().command()
