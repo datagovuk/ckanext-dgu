dacb9b9487cf4f5f17c46e7e423bd55df865a5e7
Ross Jones <ross@servercode.co.uk>
2014-01-16 10:12:52 +0000

    [1086]  Changed labels for some publisher categories.
    As per request have changed two labels whilst we get a proper classification from GDS or official gov classification.
    
    'A notional grouping of organisations' to 'A notional grouping, team or organisation'
    'Government Corporation' to 'Public Corporation'

--- ckanext/dgu/forms/validators.py
+++ ckanext/dgu/forms/validators.py
@@ -295,9 +295,9 @@ categories = (('core-department', 'UK Government Core Department'),
               ('devolved', 'Devolved Government Body'),
               ('alb', 'Arm\'s Length Body (includes Executive Agencies, Non-Departmental Public Bodies, Trading Funds and NHS bodies)'),
               ('local-council', 'Local Council'),
-              ('gov-corporation', 'Government Corporation'),
+              ('gov-corporation', 'Public Corporation'),
               ('private', 'Private Sector'),
-              ('grouping', 'A notional grouping of organisations'),
+              ('grouping', 'A notional grouping, team or organisation'),
               )
 
 def validate_publisher_category(key, data, errors, context):
