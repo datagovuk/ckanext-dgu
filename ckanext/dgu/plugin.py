datagovuk/ckanext-dgu

commit 6a230e48df1eaafb67401a610c853c2a30b91788

[1267] Added an NII icon thingy to package_read

ckanext/dgu/theme/templates/tag/index.html
@@ -2,7 +2,7 @@
   xmlns:i18n="http://genshi.edgewall.org/i18n"
   xmlns:xi="http://www.w3.org/2001/XInclude"
   py:strip="">
-  
+
   <py:def function="page_title">Tags</py:def>
   <py:match path="breadcrumbs">
     <li><a href="/tag">Tags</a></li>
@@ -44,7 +44,7 @@
 
     <div style="padding-bottom: 30px;">
       <py:for each="tag in c.page.items">
-        <a href="/data/search?tags=${tag}" class="tag">${tag}</a>
+        <a href="/data/search?tags=${tag.name}" class="tag">${tag.display_name}</a>
       </py:for>
       <div class="clearfix"></div>
     </div>

ckanext/dgu/theme/templates/_dgu_util.html
@@ -198,8 +198,6 @@
                </div>
              </div>
              <div class="right">
-                <!--! AA suggested he wanted something like this, but not enabling it so
-                      that Tom doesn't shout -->
                 <py:if test="h.is_core_dataset(package)">
                   <div class="format-box" style='background-color: #8BC658;'>NII</div>
                 </py:if>
@@ -215,12 +213,12 @@
                    <span style="display: none;" class="js-data-extent">${','.join(h.get_wms_info_extent(package))}</span>
                    ${map_preview_button_direct(package)}
                    <py:with vars="in_basket=package.id in session.get('preview_list',[])">
-                     <span 
+                     <span
                           class="preview-add js-dataset-${package.id}-add"
                           style="${if_(in_basket,'display: none;')}">
                             <button data-id="${package.id}" class="btn btn-default btn-xs btn-info btn-preview btn-basket"><i class="icon-shopping-cart"></i>&nbsp; Add to Preview List</button>
                      </span>
-                     <span 
+                     <span
                           class="preview-remove js-dataset-${package.id}-remove"
                           style="${if_(not in_basket,'display: none;')}">
                           <button data-id="${package.id}" class="btn btn-danger btn-xs btn-preview btn-basket"><i class="icon-shopping-cart"></i>&nbsp; Remove from List</button>

ckanext/dgu/theme/templates/package/read_common.html
@@ -122,6 +122,7 @@
               <span py:if="not url" rel="dc:rights"><em>&ldquo;${license_text}&rdquo;</em>${separator}</span>
             </py:if>
           </py:with>
+
           <!--
           &nbsp;
           <py:if test="isopen == True">
@@ -135,6 +136,7 @@
         </py:for>
       </span>
     </div>
+
     <div class="dataset-stars h1-subheading" style="margin-top: 0px;margin-bottom: 12px;"
       py:with="stars,reason,last_updated=h.calculate_dataset_stars(c.pkg.id)"
       >
@@ -159,7 +161,6 @@
             <i class="icon-star-empty"></i>
           </py:for>
         </a>
-
       </span>
 
       <span class="dropdown" py:if="h.is_certificates_installed() and h.has_certificate(c.pkg)" style="margin-left: 24px;">
@@ -180,6 +181,10 @@
             </div>
         </py:with>
       </span>
+
+      <div class="js-tooltip format-box pull-right" style='font-weight: bold; padding: 8px; background-color: #8BC658;' py:if="h.is_core_dataset(c.pkg_dict)" title="National Information Infrastructure">
+          <span>NII</span>
+      </div>
 
     </div>
 
@@ -210,7 +215,7 @@
         <!-- blank style tag avoids a bug in jquery.dotdotdot -->
         <div class="notes" style="">
           <div class="dataset-theme theme-${h.get_primary_theme(c.pkg_dict) or 'none'}">
-            <span class="theme-name">${h.get_primary_theme(c.pkg_dict) or '(Uncategorised)'}</span>
+            <div class="theme-name">${h.get_primary_theme(c.pkg_dict) or '(Uncategorised)'}</div>
           </div>
           <a
             class="dataset-optional-logo"
@@ -226,6 +231,7 @@
           >
             <img src="/images/uk_location.gif" />
           </a>
+
           <div class="notes-inner">
             ${h.render_markdown(c.pkg_dict.get('notes',''))}
             <py:if test="h.is_unpublished_item(c.pkg_dict)" py:with="notes=c.pkg.extras.get('release-notes','').strip()">
@@ -279,7 +285,6 @@
         <div class="panel-body">This dataset is ${deleted}. Only administrators can view it in this state.</div>
       </py:with>
     </div>
-
     ${package_content()}
   </div>
 

commit 1db350c046419c904e04154a54a88e2c26476718

[1267] Fix for tag display broken in 6a230e4. Removed /tag from the routes to make this clearer.

ckanext/dgu/plugin.py
@@ -225,6 +225,15 @@ class ThemePlugin(p.SingletonPlugin):
         return map
 
     def after_map(self, map):
+        # Delete routes to /tag since we use /data/tag and /tag is confusing to
+        # have kicking around still when it uses the same template with
+        # different inputs.
+        matches_to_delete = []
+        for match in map.matchlist:
+            if match.routepath.startswith('/tag'):
+                matches_to_delete.append(match)
+        for match in matches_to_delete:
+            map.matchlist.remove(match)
         return map
 
 

ckanext/dgu/theme/templates/tag/index.html
@@ -5,7 +5,7 @@
 
   <py:def function="page_title">Tags</py:def>
   <py:match path="breadcrumbs">
-    <li><a href="/tag">Tags</a></li>
+    <li><a href="/data/tag">Tags</a></li>
   </py:match>
 
   <div py:match="content">
@@ -44,7 +44,7 @@
 
     <div style="padding-bottom: 30px;">
       <py:for each="tag in c.page.items">
-        <a href="/data/search?tags=${tag.name}" class="tag">${tag.display_name}</a>
+        <a href="/data/search?tags=${tag}" class="tag">${tag}</a>
       </py:for>
       <div class="clearfix"></div>
     </div>