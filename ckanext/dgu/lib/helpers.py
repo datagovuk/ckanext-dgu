3e187f1c80517a85dec601c9ff5ed7d46f7d291b
Ross Jones <ross@servercode.co.uk>
2014-03-24 16:31:28 +0000

    [1396] More fixes for system update username

--- ckanext/dgu/lib/helpers.py
+++ ckanext/dgu/lib/helpers.py
@@ -243,7 +243,7 @@ def dgu_linked_user(user, maxlength=16, avatar=30, organisation=None):  # Overwr
 
     # Check if this is the site_user, and if so return 'system update' without an URL.
     site_user_name = config.get('ckan.site_id', 'ckan_site_user')
-    if user.name == site_user_name:
+    if user and user.name == site_user_name:
         return "system update"
 
     this_is_me = user and (c.user in (user.name, user.fullname))
