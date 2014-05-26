92ceb6268b3f4fb31b456d67dfa81a06b940eb17
David Read <david.read@hackneyworkshop.com>
2014-05-26 14:45:50 +0000

    [1436] Spin off ckanext-reports - Factored out the org filter to ckanext-report.

--- ckanext/dgu/lib/reports.py
+++ ckanext/dgu/lib/reports.py
@@ -110,18 +110,10 @@ def publisher_resources(organization=None,
         raise p.toolkit.ObjectNotFound('Publisher not found')
 
     # Get packages
-    if include_sub_organizations:
-        orgs = sorted([x for x in lib.go_down_tree(org)], key=lambda x: x.name)
-        org_ids = [x.id for x in orgs]
-        pkgs = model.Session.query(model.Package)\
-                .filter_by(state='active')\
-                .filter(model.Package.owner_org.in_(org_ids))\
-                .all()
-    else:
-        pkgs = model.Session.query(model.Package)\
-                .filter_by(state='active')\
-                .filter(model.Package.owner_org == org.id)\
-                .all()
+    pkgs = model.Session.query(model.Package)\
+                .filter_by(state='active')
+    pkgs = lib.filter_by_organizations(pkgs, organization,
+                                       include_sub_organizations).all()
 
     # Get their resources
     def create_row(pkg_, resource_dict):
@@ -178,114 +170,6 @@ publisher_resources_info = {
     }
 
 
-def organisation_dataset_scores(organisation_name,
-                                include_sub_organisations=False):
-    '''
-    Returns a dictionary detailing openness scores for the organisation
-    for each dataset.
-
-    i.e.:
-    {'organization_name': 'cabinet-office',
-     'organization_title:': 'Cabinet Office',
-     'table': [
-       {'package_name', 'package_title', 'resource_url', 'openness_score', 'reason', 'last_updated', 'is_broken', 'format'}
-      ...]
-
-    NB the list does not contain datasets that have 0 resources and therefore
-       score 0
-
-    '''
-    values = {}
-    sql = """
-        select package.id as package_id,
-               task_status.key as task_status_key,
-               task_status.value as task_status_value,
-               task_status.error as task_status_error,
-               task_status.last_updated as task_status_last_updated,
-               resource.id as resource_id,
-               resource.url as resource_url,
-               resource.position,
-               package.title as package_title,
-               package.name as package_name,
-               "group".id as publisher_id,
-               "group".name as publisher_name,
-               "group".title as publisher_title
-        from resource
-            left join task_status on task_status.entity_id = resource.id
-            left join resource_group on resource.resource_group_id = resource_group.id
-            left join package on resource_group.package_id = package.id
-            left join member on member.table_id = package.id
-            left join "group" on member.group_id = "group".id
-        where
-            entity_id in (select entity_id from task_status where task_status.task_type='qa')
-            and package.state = 'active'
-            and resource.state='active'
-            and resource_group.state='active'
-            and "group".state='active'
-            and task_status.task_type='qa'
-            and task_status.key='status'
-            %(org_filter)s
-        order by package.title, package.name, resource.position
-        """
-    sql_options = {}
-    org = model.Group.by_name(organisation_name)
-    if not org:
-        raise p.toolkit.ObjectNotFound('Publisher not found')
-    organisation_title = org.title
-
-    if not include_sub_organisations:
-        sql_options['org_filter'] = 'and "group".name = :org_name'
-        values['org_name'] = organisation_name
-    else:
-        sub_org_filters = ['"group".name=\'%s\'' % org.name for org in lib.go_down_tree(org)]
-        sql_options['org_filter'] = 'and (%s)' % ' or '.join(sub_org_filters)
-
-    rows = model.Session.execute(sql % sql_options, values)
-    data = dict() # dataset_name: {properties}
-    for row in rows:
-        package_data = data.get(row.package_name)
-        if not package_data:
-            package_data = OrderedDict((
-                ('dataset_title', row.package_title),
-                ('dataset_name', row.package_name),
-                ('organization_title', row.publisher_title),
-                ('organization_name', row.publisher_name),
-                # the rest are placeholders to hold the details
-                # of the highest scoring resource
-                ('resource_position', None),
-                ('resource_id', None),
-                ('resource_url', None),
-                ('openness_score', None),
-                ('openness_score_reason', None),
-                ('last_updated', None),
-                ))
-        if row.task_status_value > package_data['openness_score']:
-            package_data['resource_position'] = row.position
-            package_data['resource_id'] = row.resource_id
-            package_data['resource_url'] = row.resource_url
-
-            try:
-                package_data.update(json.loads(row.task_status_error))
-            except ValueError, e:
-                log.error('QA status "error" should have been in JSON format, but found: "%s" %s', task_status_error, e)
-                package_data['reason'] = 'Could not display reason due to a system error'
-
-            package_data['openness_score'] = row.task_status_value
-            package_data['openness_score_reason'] = package_data['reason'] # deprecated
-            package_data['last_updated'] = row.task_status_last_updated
-
-        data[row.package_name] = package_data
-
-    # Sort the results by openness_score asc so we can see the worst
-    # results first
-    data = OrderedDict(sorted(data.iteritems(),
-        key=lambda x: x[1]['openness_score']))
-
-    return {'organization_name': organisation_name,
-            'organization_title': organisation_title,
-            'table': data.values()}
-
-
 def feedback_report(organization=None, include_sub_organizations=False, include_published=False):
     """
     For the publisher provided (and optionally for sub-publishers) this
@@ -302,25 +186,13 @@ def feedback_report(organization=None, include_sub_organizations=False, include_
         organization = None
 
     # Get packages for these organization(s)
-    if organization:
-        group_ids = [organization.id]
-        if include_sub_organizations:
-            groups = sorted([x for x in lib.go_down_tree(organization)], key=lambda x: x.title)
-            group_ids = [x.id for x in groups]
-
-        memberships = model.Session.query(model.Member)\
-            .join(model.Package, model.Package.id==model.Member.table_id)\
-            .filter(model.Member.state == 'active')\
-            .filter(model.Member.group_id.in_(group_ids))\
-            .filter(model.Member.table_name == 'package')\
-            .filter(model.Package.state == 'active')
-
-    else:
-        memberships = model.Session.query(model.Member)\
-            .join(model.Package, model.Package.id==model.Member.table_id)\
-            .filter(model.Member.state == 'active')\
-            .filter(model.Member.table_name == 'package')\
-            .filter(model.Package.state == 'active')
+    memberships = model.Session.query(model.Member)\
+        .join(model.Package, model.Package.id==model.Member.table_id)\
+        .filter(model.Member.state == 'active')
+    memberships = lib.filter_by_organizations(memberships, organization,
+                                              include_sub_organizations)\
+        .filter(model.Member.table_name == 'package')\
+        .filter(model.Package.state == 'active')
 
     # For each package, count the feedback comments
     results = []
@@ -439,17 +311,10 @@ def publisher_activity(organization, include_sub_organizations=False):
     if not organization:
         pkgs = model.Session.query(model.Package)\
                 .all()
-    elif include_sub_organizations:
-        orgs = sorted([x for x in lib.go_down_tree(organization)],
-                      key=lambda x: x.title)
-        org_ids = [x.id for x in orgs]
-        pkgs = model.Session.query(model.Package)\
-                    .filter(model.Package.owner_org.in_(org_ids))\
-                    .all()
     else:
-        pkgs = model.Session.query(model.Package)\
-                    .filter(model.Package.owner_org == organization.id)\
-                    .all()
+        pkgs = model.Session.query(model.Package)
+        pkgs = lib.filter_by_organizations(pkgs, organization,
+                                           include_sub_organizations).all()
 
     for pkg in pkgs:
         created_ = model.Session.query(model.PackageRevision)\
