2d3190b1a0fc1858c68c64290cad7b326115a5ff
Tom Mortimer-Jones <tom@morty.co.uk>
2014-07-07 11:33:31 +0100

    Only show Related Applications header if there are actually related applications

--- ckanext/dgu/lib/helpers.py
+++ ckanext/dgu/lib/helpers.py
@@ -1901,10 +1901,13 @@ def relative_url_for(**kwargs):
         if not v:
             del args[k]
     return h.url_for(**args)
- 
+
 def get_related_apps(pid):
     from ckan import model
     pkg = model.Package.get(pid)
     for rel in pkg.related:
         if rel.type == 'App':
             yield rel
+
+def has_related_apps(pid):
+    return len(list(get_related_apps(pid))) > 0
