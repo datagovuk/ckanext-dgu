16b683e805fe165700d7c9ffce3a053c1471ba3b
Tom Mortimer-Jones <tom@morty.co.uk>
2014-08-01 16:09:56 +0100

    [1297] Don't use custom config file

--- ckanext/dgu/controllers/data.py
+++ ckanext/dgu/controllers/data.py
@@ -6,6 +6,7 @@ import re
 import sqlalchemy
 import urlparse
 import datetime
+import tempfile
 
 from ckanext.dgu.lib import helpers as dgu_helpers
 from ckan.lib.base import BaseController, model, abort, h
@@ -90,10 +91,8 @@ class DataController(BaseController):
                 out, err = proc.communicate()
                 exitcode = proc.returncode
                 return exitcode, out, err
-            config_paths = ','.join((os.path.join(source_repo_path, '_config.yml'),
-                                    os.path.join(source_repo_path, '_config_dgu.yml')))
-            cmd_line = '/home/co/.rvm/wrappers/ukgovldwg/jekyll build --config "%s" --source "%s" --destination "%s"'\
-                       % (config_paths, source_repo_path, build_path)
+            cmd_line = '/home/co/.rvm/wrappers/ukgovldwg/jekyll build --source "%s" --destination "%s"'\
+                       % (source_repo_path, build_path)
             c.exitcode, out, err = get_exitcode_stdout_stderr(cmd_line)
             c.stdout = cmd_line + '<br/><br/>' + out.replace('\n','<br/>')
             c.stderr = err.replace('\n','<br/>')
