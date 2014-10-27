b9055395683b4576ce0d14842126f27d3558d441
David Read <david.read@hackneyworkshop.com>
2014-10-27 17:50:59 +0000

    Revert "[1607] "Site is down for maintenance" message - now with templates."
    This reverts commit 4a3573f78d47225c61b9867633c3a46365af8de6.
    It didn't work on prod2 - I got:
    [Mon Oct 27 17:51:08 2014] [error] [client 127.0.0.1]
    self.app.setup_app_env(environ, start_response)
    [Mon Oct 27 17:51:08 2014] [error] [client 127.0.0.1] AttributeError:
    'DrupalAuthMiddleware' object has no attribute 'setup_app_env'

--- ckanext/dgu/lib/site_down_middleware.py
+++ ckanext/dgu/lib/site_down_middleware.py
@@ -1,12 +1,8 @@
-from ckan.plugins import toolkit
-
-
 class SiteDownMiddleware(object):
     def __init__(self, app, app_conf):
-        self.app = app
+        pass
 
     def __call__(self, environ, start_response):
-        # Get basic pylons going manually, so that we can render
-        self.app.setup_app_env(environ, start_response)
         start_response('503 Service Unavailable', [])
-        return toolkit.render('data/site_down.html')
+        return '<h1>Site Maintenance</h1><p>The data.gov.uk data catalogue is currently down for maintenance</p>'
+
