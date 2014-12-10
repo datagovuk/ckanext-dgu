36bd8c0d207717f3c1183da93668631fbc2d2f73
David Read <david.read@hackneyworkshop.com>
2014-12-10 17:07:20 +0000

    [1599] Auto-theme in package form - add tests for categorize_package(2).

---
+++ ckanext/dgu/tests/lib/test_theme.py
@@ -0,0 +1,32 @@
+from nose.tools import assert_equal
+
+from ckanext.dgu.lib.theme import categorize_package, categorize_package2
+
+fish_pkg = {'name': 'river fishing',
+            'title': 'fish in the river',
+            'notes':'',
+            'tags':'',
+            'extras':{}}
+
+
+class TestCategorizePackage:
+    def test_basic(self):
+        themes = categorize_package(fish_pkg)
+
+        assert_equal(type(themes), list)
+        theme = themes[0]
+        assert_equal(type(theme), str)
+        assert_equal(themes, ['Environment'])
+
+
+class TestCategorizePackage2:
+    def test_basic(self):
+        themes = categorize_package2(fish_pkg)
+
+        assert_equal(type(themes), list)
+        theme = themes[0]
+        assert_equal(type(theme), dict)
+        assert_equal(theme['name'], 'Environment')
+        # be lenient as the algorithm may change
+        assert theme['score'] > 3, theme.get('score')
+        assert theme['reasons'], theme.get('reasons')
