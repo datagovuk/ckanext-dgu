d249c2a01cf276ca55def210a01ae425f96e6319
David Read <david.read@hackneyworkshop.com>
2015-03-23 13:38:13 +0000

    [1805] Add schemas script.

--- ckanext/dgu/commands/schema.py
+++ ckanext/dgu/commands/schema.py
@@ -1,4 +1,6 @@
 import logging
+import json
+import os
 
 from ckan.lib.cli import CkanCommand
 # No other CKAN imports allowed until _load_config is run,
@@ -10,10 +12,11 @@ class Schema(CkanCommand):
 
     init - initialize the database tables
     create_test_data - create some test data (idempotent)
+    import_schemas <schemas.jsonl> - create/update schemas from a file
     """
     summary = __doc__.split('\n')[0]
     usage = __doc__
-    max_args = 1
+    max_args = 2
     min_args = 1
 
     def command(self):
@@ -21,10 +24,21 @@ class Schema(CkanCommand):
         self.log = logging.getLogger(__name__)
 
         cmd = self.args[0]
+        cmd_args = self.args[1:]
+        def assert_arg_number(expected_num_args):
+            if len(cmd_args) != expected_num_args:
+                self.parser.error('Should be %s args not %s' %
+                        (expected_num_args, len(cmd_args)))
         if cmd == 'init':
             self.init()
         elif cmd == 'create_test_data':
             self.create_test_data()
+        elif cmd == 'import_schemas':
+            assert_arg_number(1)
+            schema_filepath = cmd_args[0]
+            if not os.path.exists(schema_filepath):
+                self.parser.error('File does not exist: %s' % schema_filepath)
+            self.import_schemas(schema_filepath)
         else:
             raise NotImplementedError
 
@@ -135,3 +149,34 @@ class Schema(CkanCommand):
             dataset = pt.get_action('dataset_%s' % action)(context, dataset)
 
         print 'Datasets: ', ' '.join([dataset['name'] for dataset in datasets])
+
+    def import_schemas(self, schema_filepath):
+        from ckan import model
+        from ckanext.dgu.model.schema_codelist import Schema
+
+        # Load file with schemas
+        schema_dicts = []
+        with open(schema_filepath) as f:
+            for line in f.readlines():
+                schema_dict = json.loads(line)
+                schema_dicts.append(schema_dict)
+
+        # Create/update in the db
+        for schema in schema_dicts:
+            if 'id' in schema:
+                existing_schema = Schema.by_title(schema['title'])
+            else:
+                existing_schema = Schema.by_title(schema['title'])
+            if existing_schema:
+                schema['id'] = existing_schema.id
+                for k, v in schema.items():
+                    setattr(existing_schema, k, v)
+                schema_obj = existing_schema
+            else:
+                schema_obj = Schema(**schema)
+                model.Session.add(schema_obj)
+            model.Session.commit()
+            # Print JSONL with ids, in case you want to save with IDs
+            print json.dumps(schema_obj.as_dict())
+        model.Session.remove()
+
