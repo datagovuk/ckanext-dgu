1726f80ffc84ce26f8375c30123245e978ea2135
Tom Mortimer-Jones <tom@morty.co.uk>
2015-03-24 13:52:50 +0000

    [1805] Put DfT datasets in the right subpublisher

--- ckanext/dgu/bin/nii/dft.py
+++ ckanext/dgu/bin/nii/dft.py
@@ -21,7 +21,31 @@ with open('dft.csv') as csv_file:
             if dataset['name'] in clashing_names:
                 dataset['name'] += '_'
             dataset['notes'] = row[0]  # For now...
-            dataset['owner_org'] = 'department-for-transport'
+            publisher = row[3].strip()
+            if publisher == 'OAG':
+                dataset['owner_org'] = 'oag'
+            elif publisher == 'CAA':
+                dataset['owner_org'] = 'civil-aviation-authority'
+            elif publisher == 'Traveline':
+                dataset['owner_org'] = 'traveline-information-limited'
+            elif publisher == 'TfL / ATOC':
+                dataset['owner_org'] = 'transport-for-london'
+            elif publisher == 'OS':
+                dataset['owner_org'] = 'ordnance-survey'
+            elif publisher == 'ATOC':
+                dataset['owner_org'] = 'atoc'
+            elif publisher == 'Network Rail':
+                dataset['owner_org'] = 'network-rail'
+            elif publisher == 'DVLA':
+                dataset['owner_org'] = 'driver-and-vehicle-licensing-agency'
+            elif publisher == 'DVSA':
+                dataset['owner_org'] = 'driver-vehicle-standards-agency'
+            elif publisher == 'MCA':
+                dataset['owner_org'] = 'maritime-and-coastguard-agency'
+            elif publisher == 'CycleStreets':
+                dataset['owner_org'] = 'cycle-streets'
+            else:
+                dataset['owner_org'] = 'department-for-transport'
             dataset['theme-primary'] = 'Transport'
             dataset['core-dataset'] = 'True'  # i.e. NII
             unpublished = row[2].strip() == 'Yes'
