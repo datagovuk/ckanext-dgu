1ce1b8f38d85154929e5ecaec146e1a14be96fcf
David Read <david.read@hackneyworkshop.com>
2015-05-08 13:02:09 +0000

    [1841] Improve output. Added a single extra case - [\"None\"]

--- ckanext/dgu/bin/fix_secondary_themes_2.py
+++ ckanext/dgu/bin/fix_secondary_themes_2.py
@@ -3,73 +3,97 @@ Fix the multiple JSON encoding of the secondary theme extra
 
 e.g.
 
-"[\"Health\"]" -> ["Health"]
+'"\\"Health\\""' -> '["Health"]'
+'Towns & Cities' -> '["Towns & Cities"]'
+'"Government, Society"' -> '["Government", "Society"]'
+'None' -> '[]'
+'[\"None\"]' -> '[]'
 '''
 
-import os
-import sys
-from sqlalchemy import engine_from_config
-from pylons import config
-import common
-import ast
 import json
 from optparse import OptionParser
-from ckan import model
 
-from running_stats import StatsList
+import common
+from running_stats import Stats
 
-stats = StatsList()
+stats_format = Stats()
+stats_outcome = Stats()
 
 
 class FixSecondaryTheme2(object):
     @classmethod
-    def command(cls, config_ini, write):
-        common.load_config(config_ini)
-        common.register_translator()
+    def command(cls, options):
+        from ckan import model
+        if options.write:
+            rev = model.repo.new_revision()
+            rev.author = 'script_fix_secondary_themes_2.py'
 
-        rev = model.repo.new_revision()
-        rev.author = 'fix_secondary_theme.py'
+        datasets = common.get_datasets(state='active',
+                                       dataset_name=options.dataset)
+        for package in datasets:
+            if not 'theme-secondary' in package.extras:
+                stats_outcome.add('Ignore - no secondary theme', package.name)
+                continue
 
-        for package in model.Session.query(model.Package):
-            if 'theme-secondary' in package.extras:
-                stats.add('Testing', package.name)
+            secondary_theme = package.extras.get('theme-secondary')
 
-                secondary_theme = package.extras.get('theme-secondary')
-
-                while isinstance(secondary_theme, basestring):
-                    try:
-                        secondary_theme = json.loads(secondary_theme)
-                    except ValueError:
-                        if secondary_theme == 'None':
-                            secondary_theme = []
-                        else:
-                            secondary_theme = [t.strip() for t in secondary_theme.split(',')]
-                        break
-
-                if json.dumps(secondary_theme) != package.extras.get('theme-secondary'):
-                    stats.add('Fixing', package.name)
-                    if secondary_theme == 'None' or secondary_theme == '':
-                        package.extras['theme-secondary'] = json.dumps([])
+            loop = 1
+            while isinstance(secondary_theme, basestring):
+                try:
+                    secondary_theme = json.loads(secondary_theme)
+                except ValueError:
+                    if secondary_theme == 'None':
+                        stats_format.add('"None" string', package.name)
+                        secondary_theme = []
+                    elif ',' in secondary_theme:
+                        # e.g. '"Government, Society"'
+                        print stats_format.add('Non-JSON string, comma separated', package.name)
+                        secondary_theme = [t.strip() for t in secondary_theme.split(',')]
                     else:
-                        package.extras['theme-secondary'] = json.dumps(secondary_theme)
+                        # e.g. 'Towns & Cities'
+                        print stats_format.add('Non-JSON string', '%s %r' % (package.name, secondary_theme.strip()))
+                        secondary_theme = [secondary_theme.strip()]
+                    break
+                loop = 1
+                if loop == 2:
+                    stats_format.add('JSON', package.name)
+                elif loop == 3:
+                    # e.g. '"\\"Health\\""'
+                    print stats_format.add('Multiple JSON encoded', package.name)
 
-        if write:
+            for filter_string in (None, 'None', ''):
+                if filter_string in secondary_theme:
+                    print stats_format.add('%r in the list' % filter_string, package.name)
+                    secondary_theme = [theme for theme in secondary_theme
+                                       if theme != filter_string]
+
+            if json.dumps(secondary_theme) != package.extras.get('theme-secondary'):
+                stats_outcome.add('Fixing', package.name)
+                if secondary_theme == 'None' or secondary_theme == '':
+                    stats_format.add(secondary_theme, package.name)
+                    package.extras['theme-secondary'] = json.dumps([])
+                else:
+                    package.extras['theme-secondary'] = json.dumps(secondary_theme)
+            else:
+                stats_outcome.add('Unchanged', package.name)
+
+        print 'Formats:\n', stats_format.report()
+        print 'Outcomes:\n', stats_outcome.report()
+
+        if options.write:
+            print 'Writing...'
             model.Session.commit()
+            print '...done'
 
-        print stats.report()
 
 
-def usage():
-    print """
-Fix the multiple JSON encoding of the secondary theme extra
-
-Usage:
-
-    python fix_secondary_theme_2.py <CKAN config ini filepath>
-    """
+usage = '''
+    python %prog $CKAN_INI
+'''
 
 if __name__ == '__main__':
-    parser = OptionParser(usage='')
+    parser = OptionParser(description=__doc__.strip(), usage=usage)
+    parser.add_option('-d', '--dataset', dest='dataset')
     parser.add_option("-w", "--write",
                       action="store_true",
                       dest="write",
@@ -77,7 +101,9 @@ if __name__ == '__main__':
                       help="write the changes to the datasets")
     (options, args) = parser.parse_args()
     if len(args) != 1:
-        usage()
-        sys.exit(0)
+        parser.error('Wrong number of arguments')
     config_ini = args[0]
-    FixSecondaryTheme2.command(config_ini, options.write)
+
+    common.load_config(config_ini)
+    common.register_translator()
+    FixSecondaryTheme2.command(options)
