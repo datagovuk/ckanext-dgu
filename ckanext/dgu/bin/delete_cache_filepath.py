f47c79a1af01ffd184ea7cb0b9ca372b435eb6ba
David Read <david.read@hackneyworkshop.com>
2015-09-02 13:37:20 +0000

    [noticket] Script to remove cache_filepath extra - also wipe other legacy properties too.

--- ckanext/dgu/bin/delete_cache_filepath.py
+++ ckanext/dgu/bin/delete_cache_filepath.py
@@ -1,45 +1,107 @@
 '''
-Remove no longer used Resouce extra 'cache_filepath'
+Remove no longer used Resouce extra 'cache_filepath' and related.
 '''
 
 import common
-import json
 from optparse import OptionParser
 from ckan import model
 
-from running_stats import StatsList
+from running_stats import Stats
 
-stats = StatsList()
+stats_rp = Stats()
+stats_re = Stats()
+stats_dp = Stats()
+stats_de = Stats()
+
+# These are ckan resource properties that an old version of the archiver filled
+# in and are no longer updated. They are now stored in the Archival table.
+res_properties_to_make_null = set((
+    'cache_last_updated',
+    'size',
+    'hash',
+    'last_modified',
+    'mimetype',
+    'cache_url',
+    ))
+
+# These are custom extra fields added by old versions of the archiver &
+# qa, whereas now they store them in the Archival table.
+res_extras_to_remove = set((
+    'cache_filepath',
+    'content_length',
+    'content_type',
+    'openness_score',
+    'openness_score_failure_count',
+    'openness_score_reason',
+))
+
+dataset_properties_to_make_null = set()
+
+# These are custom extra fields added by old versions of the archiver &
+# qa, whereas now they store them in the Archival table.
+dataset_extras_to_remove = set((
+    'openness_score',
+    'openness_score_last_checked',
+    'department',  # was taken over by agency/department then owner_org
+    'agency',  # was taken over by agency/department then owner_org
+    'published_by',  # was taken over by owner_org
+    'published_via',  # was taken over by owner_org
+))
 
 
 class DeleteCacheFilepath(object):
     @classmethod
-    def command(cls, config_ini, write):
+    def command(cls, config_ini, write, options):
         common.load_config(config_ini)
         common.register_translator()
 
         rev = model.repo.new_revision()
         rev.author = 'script-delete_cache_filepath.py'
-        all_resources = model.Session.query(model.Resource) \
-                                     .join(model.ResourceGroup)\
-                                     .join(model.Package)\
-                                     .filter(model.Package.state == 'active') \
-                                     .filter(model.Package.private == False) \
-                                     .filter(model.Resource.state == 'active')
 
-        for resource in all_resources:
-            if 'cache_filepath' in resource.extras:
-                stats.add('Removing cache_filepath', resource.id)
-                del resource.extras['cache_filepath']
-            else:
-                stats.add('No cache_filepath field', resource.id)
+        # Resources
+        resources = common.get_resources_using_options(options)
+        for resource in resources:
+            for key in res_properties_to_make_null:
+                if getattr(resource, key):
+                    stats_rp.add('Making property null: %s' % key, resource.id)
+                    setattr(resource, key, None)
+                else:
+                    stats_rp.add('Property has no value: %s' % key, resource.id)
+            for key in res_extras_to_remove:
+                if key in resource.extras:
+                    stats_re.add('Removing: %s' % key, resource.id)
+                    del resource.extras[key]
+                else:
+                    stats_re.add('No field to remove: %s' % key, resource.id)
 
-        print stats.report()
+        print 'Resource Properties:\n', stats_rp.report(show_time_taken=False)
+        print 'Resource Extras:\n', stats_re.report()
+
+        # Datasets
+        if not options.resource:
+            datasets = common.get_datasets_using_options(options)
+            for dataset in datasets:
+                for key in dataset_properties_to_make_null:
+                    if getattr(dataset, key):
+                        stats_dp.add('Making property null: %s' % key, dataset.name)
+                        setattr(dataset, key, None)
+                    else:
+                        stats_dp.add('Property has no value: %s' % key, dataset.name)
+                for key in dataset_extras_to_remove:
+                    if key in dataset.extras:
+                        stats_de.add('Removing: %s' % key, dataset.name)
+                        del dataset.extras[key]
+                    else:
+                        stats_de.add('No field to remove: %s' % key, dataset.name)
+
+        print 'Dataset Properties:\n', stats_dp.report(show_time_taken=False)
+        print 'Dataset Extras:\n', stats_de.report()
 
         if write:
             print 'Writing'
             model.Session.commit()
             print 'Done'
+            stats_de.show_time_taken()
 
 usage = __doc__ + '''
 Usage:
@@ -47,6 +109,8 @@ Usage:
 
 if __name__ == '__main__':
     parser = OptionParser(usage=usage)
+    parser.add_option('-d', '--dataset', dest='dataset')
+    parser.add_option('-r', '--resource', dest='resource')
     parser.add_option("-w", "--write",
                       action="store_true",
                       dest="write",
@@ -56,4 +120,4 @@ if __name__ == '__main__':
     if len(args) != 1:
         parser.error('Wrong number of arguments')
     config_ini = args[0]
-    DeleteCacheFilepath.command(config_ini, options.write)
+    DeleteCacheFilepath.command(config_ini, options.write, options)
