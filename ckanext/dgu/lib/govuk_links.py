cca67b4e497cdc14ff21c8563e998eda6a0d4ac3
Ross Jones <ross@servercode.co.uk>
2015-10-13 15:34:21 +0100

    Update fixup to pseudo-delete resources

--- ckanext/dgu/lib/govuk_links.py
+++ ckanext/dgu/lib/govuk_links.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm.exc import DetachedInstanceError
 
 from ckanext.dgu.bin.running_stats import Stats
 from ckanext.dgu.model import govuk_publications as govuk_pubs_model
+import ckan.logic as logic
 from ckan import model
 
 
@@ -20,16 +21,74 @@ class GovukPublicationLinks(object):
             3. We do not have a Publication that shares a URL with this resource - call function
                 to create it.
         """
+        stats = Stats()
         results = get_packages_and_resources(resource_id=resource_id,
                                                                         dataset_name=dataset_name,
                                                                         url_like="https://www.gov.uk/government/publications/%")
+
+        resource_deletion = defaultdict(list)
 
         res_count = 0
         for _, v in results.iteritems():
             res_count += len(v)
         print '%i broken packages with %i broken resources that point to a publication page on GOV.UK' % (len(results), res_count,)
 
+        for pkg_id, resource_ids in results.iteritems():
+            pkg = model.Package.get(pkg_id)
 
+            for resource_id in resource_ids:
+                # Is there a publication with the same URL?
+                publication_link = publication_link_from_resource(resource_id)
+
+                if publication_link:
+                    # This resource points at a publication
+                    publication_dataset_link = publication_link_to_dataset(pkg.id, publication_link.govuk_id)
+
+                    if publication_dataset_link:
+                        # The publication link for the resource points to a valid dataset
+                        stats.add("Has a link to a publication which references a dataset", resource_id)
+
+                        if publication_dataset_link.ckan_id != pkg.id:
+                            # The dataset the publication references isn't the same one that the resource is in.
+                            # Leave the scraper to import it into the right place.
+                            stats.add("Resource references publication, but resource is in wrong dataset", resource_id)
+                            resource_deletion[pkg.name].append(resource_id)
+
+                    else:
+                        # The publication link for the resource does not point at a dataset
+                        stats.add("Has a link to a publication which DOES NOT reference a dataset", resource_id)
+                        # TODO: Try resource.resource_group.package as the link ?
+                        # TODO: Delete the resource
+
+                else:
+                        # This resource does not point at a publication, forward that URL for scraping.
+                        # Either:
+                        #   1. The publication isn't listed in a place where we can scrape
+                        #   2. It no longer exists.
+                        stats.add("Does not have a link to a publication", resource_id)
+
+                        # We should probably delete this resource.
+                        resource_deletion[pkg.name].append(resource_id)
+
+
+        # For each entry in resource_deletion, we will call package_show and then modify the dict
+        # before calling package_update.
+        context = {'model': model, 'session': model.Session}
+        for k, v in resource_deletion.iteritems():
+            print "Checking in {p} for resources to delete".format(p=k)
+            pkg_dict = logic.get_action('package_show')(context, {'id': k})
+            print "Was", len(pkg_dict['resources'])
+
+            new_resources = []
+            for res_dict in pkg_dict.get('resources'):
+                if res_dict['id'] not in v:
+                    print "Deleting resource ", res_dict['id']
+                    new_resources.append(res_dict)
+            pkg_dict['resources'] = new_resources
+
+            print len(pkg_dict['resources'])
+            break
+        print stats
 
     @classmethod
     def autolink(cls, resource_id=None, dataset_name=None):
@@ -102,8 +161,23 @@ class GovukPublicationLinks(object):
                 objs_to_link.extend([(govuk_type, obj) for obj in objs_to_link_])
         return objs_to_link
 
+
+def publication_link_to_dataset(pkg_id, pub_id):
+    return  model.Session.query(govuk_pubs_model.Link)\
+            .filter(govuk_pubs_model.Link.ckan_id == pkg_id )\
+            .filter(govuk_pubs_model.Link.ckan_table == "package")\
+            .filter(govuk_pubs_model.Link.govuk_table == "publication")\
+            .filter(govuk_pubs_model.Link.govuk_id == pub_id).first()
+
+
+def publication_link_from_resource(resource_id):
+    return model.Session.query(govuk_pubs_model.Link)\
+                        .filter(govuk_pubs_model.Link.ckan_id == resource_id )\
+                        .filter(govuk_pubs_model.Link.ckan_table == "resource")\
+                        .filter(govuk_pubs_model.Link.govuk_table == "publication").first()
+
 def get_packages_and_resources(resource_id=None, dataset_name=None, url_like='https:\/\/www.gov.uk\/%'):
-    ''' Returns all gov.uk resources, or filtered by the given criteria. '''
+    ''' Returns all gov.uk resource ids grouped by the package_ids.'''
     from ckan import model
     resources = model.Session.query(model.Resource.id, model.Package.id) \
                 .filter_by(state='active') \
