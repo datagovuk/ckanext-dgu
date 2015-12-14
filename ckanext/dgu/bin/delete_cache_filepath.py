f8bbf42ee5d0e239b451cff5e3c524728c723941
Ross Jones <ross@servercode.co.uk>
2015-12-14 14:38:17 +0000

    Try and streamline the clearing of resource and dataset extras

--- ckanext/dgu/bin/delete_cache_filepath.py
+++ ckanext/dgu/bin/delete_cache_filepath.py
@@ -1,6 +1,8 @@
 '''
 Remove no longer used Resouce extra 'cache_filepath' and related.
 '''
+import math
+from copy import deepcopy
 
 import common
 from optparse import OptionParser
@@ -50,6 +52,30 @@ dataset_extras_to_remove = set((
 
 
 class DeleteCacheFilepath(object):
+
+    @classmethod
+    def process_resource(self, resource):
+        updated = False
+        for key in res_properties_to_make_null:
+            if getattr(resource, key):
+                stats_rp.add('Making property null: %s' % key, resource.id)
+                setattr(resource, key, None)
+                updated = True
+            else:
+                stats_rp.add('Property has no value: %s' % key, resource.id)
+
+        extras = deepcopy(resource.extras)
+        for key in res_extras_to_remove:
+            if key in extras:
+                stats_re.add('Removing: %s' % key, resource.id)
+                del extras[key]
+                updated = True
+            else:
+                stats_re.add('No field to remove: %s' % key, resource.id)
+        if updated:
+            resource.extras = extras
+        return updated
+
     @classmethod
     def command(cls, config_ini, write, options):
         common.load_config(config_ini)
@@ -58,50 +84,58 @@ class DeleteCacheFilepath(object):
         rev = model.repo.new_revision()
         rev.author = 'script-delete_cache_filepath.py'
 
-        # Resources
-        resources = common.get_resources_using_options(options)
-        for resource in resources:
-            for key in res_properties_to_make_null:
-                if getattr(resource, key):
-                    stats_rp.add('Making property null: %s' % key, resource.id)
-                    setattr(resource, key, None)
-                else:
-                    stats_rp.add('Property has no value: %s' % key, resource.id)
-            for key in res_extras_to_remove:
-                if key in resource.extras:
-                    stats_re.add('Removing: %s' % key, resource.id)
-                    del resource.extras[key]
-                else:
-                    stats_re.add('No field to remove: %s' % key, resource.id)
+        process_all = True
+        if options.resource:
+            cls.process_resource(model.Resource.get(options.resource))
+            process_all = False
+        else:
+            # Get each dataset,
+            counter = 0
+            datasets_q =  model.Session.query(model.Package) \
+                    .filter_by(state='active')
+
+            rounded = int(math.ceil(datasets_q.count() / 100.0)) * 100
+            for x in xrange(0, rounded, 100):
+                datasets = datasets_q.offset(x).limit(100)
+                updated = False
+
+                for dataset in datasets.all():
+                    counter += 1
+
+                    print "Processing dataset %d\r" % counter,
+                    for resource in dataset.resources:
+                        if cls.process_resource(resource):
+                            updated = True
+
+                    for key in dataset_properties_to_make_null:
+                        if getattr(dataset, key):
+                            stats_dp.add('Making property null: %s' % key, dataset.name)
+                            setattr(dataset, key, None)
+                            updated = True
+                        else:
+                            stats_dp.add('Property has no value: %s' % key, dataset.name)
+
+                    for key in dataset_extras_to_remove:
+                        if key in dataset.extras:
+                            #stats_de.add('Removing: %s' % key, dataset.name)
+                            del dataset.extras[key]
+                            updated = True
+                        else:
+                            stats_de.add('No field to remove: %s' % key, dataset.name)
+
+                # We will be committing 100 at a time
+                if updated and write:
+                    print "\nCommitting changes"
+                    import time
+                    s = time.time()
+                    model.Session.commit()
+                    print "Committed in ", time.time() - s
 
         print 'Resource Properties:\n', stats_rp.report(show_time_taken=False)
         print 'Resource Extras:\n', stats_re.report()
-
-        # Datasets
-        if not options.resource:
-            datasets = common.get_datasets_using_options(options)
-            for dataset in datasets:
-                for key in dataset_properties_to_make_null:
-                    if getattr(dataset, key):
-                        stats_dp.add('Making property null: %s' % key, dataset.name)
-                        setattr(dataset, key, None)
-                    else:
-                        stats_dp.add('Property has no value: %s' % key, dataset.name)
-                for key in dataset_extras_to_remove:
-                    if key in dataset.extras:
-                        stats_de.add('Removing: %s' % key, dataset.name)
-                        del dataset.extras[key]
-                    else:
-                        stats_de.add('No field to remove: %s' % key, dataset.name)
-
         print 'Dataset Properties:\n', stats_dp.report(show_time_taken=False)
         print 'Dataset Extras:\n', stats_de.report()
 
-        if write:
-            print 'Writing'
-            model.Session.commit()
-            print 'Done'
-            stats_de.show_time_taken()
 
 usage = __doc__ + '''
 Usage:
