29bd12ac303d60a98c4c2548bbfeaa8b572933a4
Ross Jones <ross@servercode.co.uk>
2016-01-29 11:21:46 +0000

    Fix the collection index page and action links

--- ckanext/dgu/logic/action/update.py
+++ ckanext/dgu/logic/action/update.py
@@ -2,7 +2,13 @@
 
 
 def add_to_collection(context, data_dict):
-    data_dict = {"id": new_group,
+    '''
+    Adds a dataset to a collection.
+
+    dataset_id : The ID of a dataset
+    collection_id: The ID of a collection
+
+    data_dict = {"id": data_dict[''],
                  "object": id,
                  "object_type": 'package',
                  "capacity": 'public'}
@@ -10,8 +16,15 @@ def add_to_collection(context, data_dict):
         get_action('member_create')(context, data_dict)
     except NotFound:
         abort(404, _('Group not found'))
+    '''
 
 def remove_from_collection(context, data_dict):
+    '''
+    Removes a dataset from a collection.
+
+    dataset_id : The ID of a dataset
+    collection_id: The ID of a collection
+
     data_dict = {"id": new_group,
                  "object": id,
                  "object_type": 'package',
@@ -20,3 +33,5 @@ def remove_from_collection(context, data_dict):
         get_action('member_create')(context, data_dict)
     except NotFound:
         abort(404, _('Group not found'))
+    '''
+
