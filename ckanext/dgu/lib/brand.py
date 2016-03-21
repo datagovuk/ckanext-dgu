6e940c25a33c5c3da5ebef3d23e74e444caceca1
Ross Jones <ross@servercode.co.uk>
2016-03-21 10:18:30 +0000

    Change the styling on the search results page, currently breaks publisher page

---
+++ ckanext/dgu/lib/brand.py
@@ -0,0 +1,49 @@
+
+core_brands = {
+"attorney-generals-office": "#a03a88",
+    "cabinet-office": "#347da4",
+# $civil-service: #af292e;
+"department-for-business-innovation-and-skills": "#347da4",
+"department-for-communities-and-local-government": "#37836e",
+"department-for-culture-media-sport": "#a03155",
+"department-for-education": "#347ca9",
+"department-for-environment-food-and-rural-affairs": "#00a33b",
+"department-for-international-development": "#405e9a",
+"department-for-transport": "#398373",
+"department-for-work-and-pensions": "#37807b",
+"department-of-energy-and-climate-change": "#2b7cac",
+"department-of-health": "#39836e",
+"foreign-commonwealth-office": "#406e97",
+$government-equalities-office: #9325b2;
+$hm-government: #0076c0;
+$hm-government-websafe: #347da4;
+$hm-revenue-customs: #009390;
+$hm-revenue-customs-websafe: #008770;
+$hm-treasury: #af292e;
+$hm-treasury-websafe: #832322;
+$home-office: #9325b2;
+$home-office-websafe: #9440b2;
+$ministry-of-defence: #4d2942;
+$ministry-of-defence-websafe: #5a5c92;
+$ministry-of-justice: #231f20;
+$ministry-of-justice-websafe: #5a5c92;
+$northern-ireland-office: #002663;
+$northern-ireland-office-websafe: #3e598c;
+$office-of-the-advocate-general-for-scotland: #002663;
+$office-of-the-advocate-general-for-scotland-websafe: $link-colour;
+$office-of-the-leader-of-the-house-of-lords: #9c132e;
+$office-of-the-leader-of-the-house-of-lords-websafe: #c2395d;
+$scotland-office: #002663;
+$scotland-office-websafe: #405c8a;
+// Note: the 'the' part here will get dropped
+$the-office-of-the-leader-of-the-house-of-commons: #317023;
+$the-office-of-the-leader-of-the-house-of-commons-websafe: #005f8f;
+$uk-export-finance: #005747;
+$uk-export-finance-websafe: $link-colour;
+$uk-trade-investment: #C80651;
+$uk-trade-investment-websafe: $link-colour;
+$wales-office: #a33038;
+$wales-office-websafe: #7a242a;
+
+}
+
